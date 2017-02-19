64f5703267a8aa28f1a01cdf3b6be179de8931dd
PPYAPgit <ppyapmail@gmail.com>
2017-02-19 16:12:51 +0900

    Post detail view is now available

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,3 +7,13 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+        if post.published_date and post.published_date <= timezone.now():
+            return render(request, 'blog/post_detail.html', {'post': post})
+        else:
+            raise Http404()
+    except Http404:
+        raise Http404()
